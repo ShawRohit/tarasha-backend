8ddb05e5c87428dea562b69abfdaea3d83463596
Rohit Shaw <rohit.shaw.ku500@gmail.com>
2024-05-17 11:29:38 +0530

    cors removed

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = 3000;
-const cors = require('cors');
+const cors = require("cors");
 // const swaggerUi = require('swagger-ui-express');
 // const swaggerFile = require('./swagger_output.json');
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 // const cookieParser = require('cookie-parser');
-const formattedResponse = require('./src/utils/formatted-response');
-require('dotenv').config();
-const connectDB = require('./config/db');
+const formattedResponse = require("./src/utils/formatted-response");
+require("dotenv").config();
+const connectDB = require("./config/db");
 connectDB();
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,20 +17,20 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://tarasha-demo.onrender.com'],
-    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
   })
 );
 app.use(
-  '/api/user',
+  "/api/user",
   formattedResponse,
-  require('./src/user/routes/user.authorizationroutes')
+  require("./src/user/routes/user.authorizationroutes")
 );
 app.use(
-  '/api/calendar',
+  "/api/calendar",
   formattedResponse,
-  require('./src/calendar/routes/calendar')
+  require("./src/calendar/routes/calendar")
 );
-app.use('/api/blogs', require('./src/blog/routes/blogs.routes'));
+app.use("/api/blogs", require("./src/blog/routes/blogs.routes"));
 module.exports = app;
